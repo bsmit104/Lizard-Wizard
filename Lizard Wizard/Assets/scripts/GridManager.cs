8f7334a22588b45862e41ec25453eeba6d204981
jwvonk <jwavonk@gmail.com>
2024-06-09 22:56:14 -0700

    New Tileset

--- Lizard Wizard/Assets/scripts/GridManager.cs
+++ Lizard Wizard/Assets/scripts/GridManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -29,28 +28,65 @@ public class GridManager : MonoBehaviour
     char[,] GenerateGrid(int numRows, int numCols)
     {
         char[,] grid = new char[numRows, numCols];
-        for (int i = 0; i < numRows; i++)
+
+        bool shelfBreak = true;
+        //bool prevDungeon = true;
+        float bookcasePickerY = Mathf.Floor(Random.value * 100f);
+        float columnPickerY;
+        for (int j = 0; j < numRows; j++)
         {
-            for (int j = 0; j < numCols; j++)
+            float zonePicker = Mathf.PerlinNoise(0, j / 10f);
+            if (zonePicker < .6f)
             {
-                float n = Mathf.PerlinNoise(i / 10f, j / 10f);
-                if (n < 0.35f)
+                // Library
+                for (int i = 0; i < numCols; i++)
                 {
-                    grid[i, j] = '~'; // Water
+                    if (!(Mathf.PerlinNoise(0, (j + 1) / 10f) < 0.6f)) // next one is Dungeon
+                    {
+                        grid[i, j] = '#'; // Floor
+                    }
+                    else if (shelfBreak)
+                    {
+                        grid[i, j] = '-'; // Wall
+                    }
+                    else
+                    {
+                        float bookcasePicker = Mathf.PerlinNoise(i / 5f, bookcasePickerY / 5f);
+                        if (bookcasePicker < 0.6f)
+                        {
+                            grid[i, j] = '='; // Bookcase
+                        }
+                        else
+                        {
+                            grid[i, j] = '-'; // Wood Wall
+                        }
+                    }
                 }
-                else if (n < 0.4f)
+                shelfBreak = false;
+                if (Random.value < .05)
                 {
-                    grid[i, j] = '='; // Sand
+                    bookcasePickerY = Mathf.Floor(Random.value * 100f);
+                    shelfBreak = true;
                 }
-                else if (n < 0.5f)
-                {
-                    grid[i, j] = '_'; // Grass
-                }
-                else
-                {
-                    grid[i, j] = '.'; // Ground
-                }
+                columnPickerY = Mathf.Floor(Random.value * 100f);
             }
+            else
+            {
+                for (int i = 0; i < numCols; i++)
+                {
+                    float columnPicker = Mathf.PerlinNoise(i / 5f, bookcasePickerY / 5f);
+                    if (columnPicker < 0.6f)
+                    {
+                        grid[i, j] = '$'; // Light Wall
+                    }
+                    else
+                    {
+                        grid[i, j] = '%'; // Dark Wall
+                    }
+                }
+                shelfBreak = true;
+            }
+
         }
         return grid;
     }
@@ -77,24 +113,12 @@ public class GridManager : MonoBehaviour
         code += GridCheck(grid, i, j + 1, targets) ? 1 << 1 : 0;
         code += GridCheck(grid, i + 1, j, targets) ? 1 << 2 : 0;
         code += GridCheck(grid, i - 1, j, targets) ? 1 << 3 : 0;
-        code += GridCheck(grid, i + 1, j - 1, targets) ? 1 << 4 : 0;
-        code += GridCheck(grid, i - 1, j - 1, targets) ? 1 << 5 : 0;
-        code += GridCheck(grid, i + 1, j + 1, targets) ? 1 << 6 : 0;
-        code += GridCheck(grid, i - 1, j + 1, targets) ? 1 << 7 : 0;
         return code;
     }
 
     void DrawContext(char[,] grid, int i, int j, char[] targets, int ti, int tj)
     {
         int code = NeighborsCode(grid, i, j, targets);
-        // Render outside corners
-        for (int k = 0; k < 4; k++)
-        {
-            int mask = 1 << (4 + k);
-            int index = code & mask;
-            Vector2Int offsets = lookup[index][0];
-            PlaceTile(i, j, ti + offsets.x, tj + offsets.y);
-        }
 
         // Render inside corners/edges
         foreach (var offsets in lookup[code & 15])
@@ -108,7 +132,7 @@ public class GridManager : MonoBehaviour
 
     void InitializeLookup()
     {
-        for (int k = 0; k < 256; k++)
+        for (int k = 0; k < 16; k++)
         {
             lookup[k] = new List<Vector2Int> { new Vector2Int(1, 1) };
         }
@@ -128,184 +152,71 @@ public class GridManager : MonoBehaviour
         lookup[13] = new List<Vector2Int> { new Vector2Int(2, 0), new Vector2Int(0, 0) }; // North + East + West
         lookup[14] = new List<Vector2Int> { new Vector2Int(2, 2), new Vector2Int(0, 2) }; // South + East + West
         lookup[15] = new List<Vector2Int> { new Vector2Int(2, 0), new Vector2Int(0, 0), new Vector2Int(1, 2) }; // North + South + East + West
-        lookup[16] = new List<Vector2Int> { new Vector2Int(3, 1) }; // Northeast
-        lookup[32] = new List<Vector2Int> { new Vector2Int(4, 1) }; // Northwest
-        lookup[64] = new List<Vector2Int> { new Vector2Int(3, 0) }; // Southeast
-        lookup[128] = new List<Vector2Int> { new Vector2Int(4, 0) }; // Southwest
-    }
-
-
-
-    // Define sand macro tiles and alt sand macro tiles
-    List<List<List<Vector2Int>>> sandMacroTiles = new List<List<List<Vector2Int>>>
-    {
-        new List<List<Vector2Int>>
-        {
-            new List<Vector2Int> { new Vector2Int(1, 18), new Vector2Int(3, 18), new Vector2Int(0, 18) },
-            new List<Vector2Int> { new Vector2Int(1, 20), new Vector2Int(0, 18), new Vector2Int(0, 18) },
-            new List<Vector2Int> { new Vector2Int(0, 18), new Vector2Int(0, 18), new Vector2Int(0, 18) }
-        },
-        new List<List<Vector2Int>>
-        {
-            new List<Vector2Int> { new Vector2Int(0, 18), new Vector2Int(1, 18), new Vector2Int(3, 18) },
-            new List<Vector2Int> { new Vector2Int(0, 18), new Vector2Int(1, 20), new Vector2Int(0, 18) },
-            new List<Vector2Int> { new Vector2Int(0, 18), new Vector2Int(0, 18), new Vector2Int(0, 18) }
-        },
-        new List<List<Vector2Int>>
-        {
-            new List<Vector2Int> { new Vector2Int(0, 18), new Vector2Int(0, 18), new Vector2Int(0, 18) },
-            new List<Vector2Int> { new Vector2Int(0, 18), new Vector2Int(1, 18), new Vector2Int(3, 18) },
-            new List<Vector2Int> { new Vector2Int(0, 18), new Vector2Int(1, 20), new Vector2Int(0, 18) }
-        },
-        new List<List<Vector2Int>>
-        {
-            new List<Vector2Int> { new Vector2Int(0, 18), new Vector2Int(0, 18), new Vector2Int(0, 18) },
-            new List<Vector2Int> { new Vector2Int(1, 18), new Vector2Int(3, 18), new Vector2Int(0, 18) },
-            new List<Vector2Int> { new Vector2Int(1, 20), new Vector2Int(0, 18), new Vector2Int(0, 18) }
-        },
-        new List<List<Vector2Int>>
-        {
-            new List<Vector2Int> { new Vector2Int(1, 18), new Vector2Int(3, 18), new Vector2Int(0, 18) },
-            new List<Vector2Int> { new Vector2Int(1, 20), new Vector2Int(1, 18), new Vector2Int(3, 18) },
-            new List<Vector2Int> { new Vector2Int(0, 18), new Vector2Int(1, 20), new Vector2Int(0, 18) }
-        },
-        new List<List<Vector2Int>>
-        {
-            new List<Vector2Int> { new Vector2Int(0, 18), new Vector2Int(1, 18), new Vector2Int(3, 18) },
-            new List<Vector2Int> { new Vector2Int(1, 18), new Vector2Int(3, 19), new Vector2Int(0, 18) },
-            new List<Vector2Int> { new Vector2Int(1, 20), new Vector2Int(0, 18), new Vector2Int(0, 18) }
-        }
-    };
-
-    List<List<List<Vector2Int>>> sandAltMacroTiles = new List<List<List<Vector2Int>>>
-    {
-        new List<List<Vector2Int>>
-        {
-            new List<Vector2Int> { new Vector2Int(9, 18), new Vector2Int(11, 18), new Vector2Int(0, 19) },
-            new List<Vector2Int> { new Vector2Int(9, 20), new Vector2Int(0, 19), new Vector2Int(0, 19) },
-            new List<Vector2Int> { new Vector2Int(0, 19), new Vector2Int(0, 19), new Vector2Int(0, 19) }
-        },
-        new List<List<Vector2Int>>
-        {
-            new List<Vector2Int> { new Vector2Int(0, 19), new Vector2Int(9, 18), new Vector2Int(11, 18) },
-            new List<Vector2Int> { new Vector2Int(0, 19), new Vector2Int(9, 20), new Vector2Int(0, 19) },
-            new List<Vector2Int> { new Vector2Int(0, 19), new Vector2Int(0, 19), new Vector2Int(0, 19) }
-        },
-        new List<List<Vector2Int>>
-        {
-            new List<Vector2Int> { new Vector2Int(0, 19), new Vector2Int(0, 19), new Vector2Int(0, 19) },
-            new List<Vector2Int> { new Vector2Int(0, 19), new Vector2Int(9, 18), new Vector2Int(11, 18) },
-            new List<Vector2Int> { new Vector2Int(0, 19), new Vector2Int(9, 20), new Vector2Int(0, 19) }
-        },
-        new List<List<Vector2Int>>
-        {
-            new List<Vector2Int> { new Vector2Int(0, 19), new Vector2Int(0, 19), new Vector2Int(0, 19) },
-            new List<Vector2Int> { new Vector2Int(9, 18), new Vector2Int(11, 18), new Vector2Int(0, 19) },
-            new List<Vector2Int> { new Vector2Int(9, 20), new Vector2Int(0, 19), new Vector2Int(0, 19) }
-        },
-        new List<List<Vector2Int>>
-        {
-            new List<Vector2Int> { new Vector2Int(9, 18), new Vector2Int(11, 18), new Vector2Int(0, 19) },
-            new List<Vector2Int> { new Vector2Int(9, 20), new Vector2Int(9, 18), new Vector2Int(11, 18) },
-            new List<Vector2Int> { new Vector2Int(0, 19), new Vector2Int(9, 20), new Vector2Int(0, 19) }
-        },
-        new List<List<Vector2Int>>
-        {
-            new List<Vector2Int> { new Vector2Int(0, 19), new Vector2Int(9, 18), new Vector2Int(11, 18) },
-            new List<Vector2Int> { new Vector2Int(9, 18), new Vector2Int(11, 19), new Vector2Int(0, 19) },
-            new List<Vector2Int> { new Vector2Int(9, 20), new Vector2Int(0, 19), new Vector2Int(0, 19) }
-        }
-    };
-
-
-    void PlaceMacroTile(char[,] grid, int i, int j, char[] targets, bool alt)
-    {
-        var macroTiles = alt ? sandAltMacroTiles : sandMacroTiles;
-        var macroTile = GetRandomMacroTile(macroTiles);
-
-        for (int jOffset = 0; jOffset < 3; jOffset++)
-        {
-            for (int iOffset = 0; iOffset < 3; iOffset++)
-            {
-                if (GridCheck(grid, i + iOffset, j + jOffset, targets))
-                {
-                    Vector2Int offsets = macroTile[jOffset][iOffset];
-                    PlaceTile(i + iOffset, j + jOffset, offsets[0], offsets[1]);
-                }
-            }
-        }
-    }
-
-    List<List<Vector2Int>> GetRandomMacroTile(List<List<List<Vector2Int>>> macroTiles)
-    {
-        return macroTiles[Random.Range(0, macroTiles.Count)];
     }
 
     void DrawGrid(char[,] grid)
     {
         for (int j = 0; j < grid.GetLength(0); j++)
         {
+            float shelfPicker = Random.value;
+            int floorXOffset = Random.Range(0, 3);
+            int wallXOffset = Random.Range(0, 4);
             for (int i = 0; i < grid.GetLength(1); i++)
             {
-                // Draw Sand and AltSand Macro Tiles
-                if (i % 3 == 0 && j % 3 == 0)
+                // Library
+                if (GridCheck(grid, i, j, new char[] {'#'}))
                 {
-                    PlaceMacroTile(grid, i, j, new char[] { '_' }, true);
-                    PlaceMacroTile(grid, i, j, new char[] { '.' }, false);
+                    PlaceTile(i, j, 7 + floorXOffset, 1);
+                    floorXOffset = (floorXOffset + 1) % 3;
                 }
-                // Let Sand and Grass grow over AltSand
-                if (GridCheck(grid, i, j, new char[] { '_' }))
+                if (GridCheck(grid, i, j, new char[] { '-' }))
                 {
-                    DrawContext(grid, i, j, new char[] { '=' }, 4, 0);
-                    DrawContext(grid, i, j, new char[] { '.' }, 4, 18);
-                }
-                // Grass
-                if (GridCheck(grid, i, j, new char[] { '=' }))
-                {
-                    int tileX;
-                    if (Random.value > 0.2f)
-                    {
-                        tileX = 0; // No Leaves
-                        var foo = Random.value; // keep same amount of random calls on each iteration
-                    }
-                    else
-                    {
-                        tileX = Random.Range(1, 4); // Random Leaf Tile
-                    }
+                    int tileX = Random.Range(0, 4); // Random Wood Wall Tile
                     PlaceTile(i, j, tileX, 0);
-                    DrawContext(grid, i, j, new char[] { '.' }, 4, 18);
-
-                    if (Random.value < 0.05f)
-                    {
-                        PlaceTile(i, j, 14, 0); // Tree
-                    }
-
-                    if (Random.value < 0.02f)
-                    {
-                        PlaceTile(i, j, 28, 1); // Tower Base
-                        if (j - 1 != -1)
-                        {
-                            // Tile above isn't offscreen
-                            PlaceTile(i, j - 1, 28, 0); // Tower Spire
-                        }
-                    }
                 }
-                // Water
-                if (GridCheck(grid, i, j, new char[] { '~' }))
+                if (GridCheck(grid, i, j, new char[] { '=' })) {
+                    if (shelfPicker < 0.7)
+                    {
+                        int tileX = Random.Range(10, 12);
+                        if (Random.value < 0.9)
+                        {
+                            PlaceTile(i, j, tileX, 0);
+                        }
+                        else
+                        {
+                            PlaceTile(i, j, tileX, 1);
+                        }
+
+                    } else
+                    {
+                        int tileX = Random.Range(0, 3); // Random Bookshelf Wall Tile
+                        PlaceTile(i, j, tileX, 1);
+                    }
+
+                    // Wood Wall & Floor into Bookshelf
+                    DrawContext(grid, i, j, new char[] { '-', '#' }, 4, 0);
+                }
+
+                if (GridCheck(grid, i, j, new char[] { '%' }))
                 {
-                    int tileX;
-                    if (Mathf.PerlinNoise(i + Time.time / 10000f, j + Time.time / 1000f) > 0.16f)
+                    // Light Stone Wall
+                    PlaceTile(i, j, wallXOffset, 4);
+                    wallXOffset = (wallXOffset + 1) % 4;
+
+                }
+
+                if (GridCheck(grid, i, j, new char[] { '$' }))
+                {
+                    // Dark Stone Wall
+                    PlaceTile(i, j, wallXOffset, 3);
+                    wallXOffset = (wallXOffset + 1) % 4;
+                    // Everything into stone wall
+                    if (Random.value < 0.01f)
                     {
-                        tileX = 0; // No Bubbles
-                        var foo = Random.value; // keep same amount of random calls on each iteration
+                        PlaceTile(i, j, 9, 3); // Skull
                     }
-                    else
-                    {
-                        tileX = Random.Range(1, 4); // Random Bubble Tile
-                    }
-                    PlaceTile(i, j, tileX, 14);
-                    // Convert water to Canal next to Land
-                    DrawContext(grid, i, j, new char[] { '=', '_', '.' }, 5, 21);
+                    DrawContext(grid, i, j, new char[] { '-', '#', '%' }, 4, 3);
+
                 }
             }
         }
@@ -314,7 +225,7 @@ public class GridManager : MonoBehaviour
     void PlaceTile(int targetX, int targetY, int paletteX, int paletteY)
     {
         Vector3Int targetPos = new Vector3Int(targetX, -targetY, 0);
-        Vector3Int palettePos = new Vector3Int(paletteX + 2, -paletteY - 1, 0);
+        Vector3Int palettePos = new Vector3Int(paletteX - 2, 14 - paletteY, 0);
 
         //int zLevel = 0;
         bool tileFound = false;
@@ -331,5 +242,4 @@ public class GridManager : MonoBehaviour
             }
         }
     }
-
 }
